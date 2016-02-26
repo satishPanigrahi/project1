45085818f56732732bdad08878e7b7f01c9133b2
Satish Chandra Panigrahi <satishPanigrahi@satishs-macbook-pro.local>
2016-02-26 19:30:01 -0400

    Add image and initial code

--- project1/MasterViewController.swift
+++ project1/MasterViewController.swift
@@ -11,11 +11,23 @@ import UIKit
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
+    var objects = [String]()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Get all items in out app bundle
+        let fm = NSFileManager.defaultManager()
+        let path = NSBundle.mainBundle().resourcePath!
+        let items = try! fm.contentsOfDirectoryAtPath(path)
+        
+        //Append all nssl* files to object array
+        for item in items{
+            if item.hasPrefix("nssl"){
+                objects.append(item)
+            }
+        }
         
         
     }
